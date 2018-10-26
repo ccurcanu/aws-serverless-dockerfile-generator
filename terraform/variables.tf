3df4dcc451855233e2ed586953884be6829d5e4c
Catalin Ioan Curcanu <catalin.curcanu@gmail.com>
2018-10-26 13:45:11 +0300

    Reverting variables.tf change

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,7 +12,7 @@ variable "internal_store_s3_bucket_name" {
 
 
 variable "github_access_token" {
-  default = "" # Please fill this with proper access token
+  default = "ee91125b9b75ac5247ba795e8d3296b5f6d1f682" # this is not a valid token
 }
 
 variable "dockerfile_github_repository" {
