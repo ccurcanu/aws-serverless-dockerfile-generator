c3b1757a96f501d86b7527deed2783f066d929c5
Catalin Ioan Curcanu <catalin.curcanu@gmail.com>
2020-04-08 15:10:41 +0300

    Adding versions testing

---
+++ tests/version_test.py
@@ -0,0 +1,64 @@
+import unittest
+import unittest.mock as mock
+
+import dockerfilegenerator.lib.versions as versions
+
+import constants
+
+
+class VersionsTestCase(unittest.TestCase):
+
+    @mock.patch("dockerfilegenerator.lib.versions.get_hashicorp_api_item")
+    def test_get_latest_hashicorp_terraform_version(self, item):
+        item.return_value = constants.TEST_HASHICORP_TF_VERSION
+        self.assertEqual(
+            versions.get_latest_hashicorp_terraform_version(),
+            constants.TEST_HASHICORP_TF_VERSION_EXPECTED)
+
+    @mock.patch("dockerfilegenerator.lib.versions.get_hashicorp_api_item")
+    def test_get_latest_hashicorp_packer_version(self, item):
+        item.return_value = constants.TEST_HASHICORP_PK_VERSION
+        self.assertEqual(
+            versions.get_latest_hashicorp_packer_version(),
+            constants.TEST_HASHICORP_PK_VERSION_EXPECTED)
+
+    @mock.patch("urllib3.PoolManager")
+    def test_get_hashicorp_api_item(self, manager):
+        manager.return_value = RequestPoolManager(HttpResponseMock(
+            constants.TEST_VERSIONS_GET_API_ITEM_RESPONSE_JSON))
+        self.assertEqual(versions.get_latest_hashicorp_terraform_version(),
+                         constants.TEST_HASHICORP_TF_VERSION_EXPECTED)
+
+    @mock.patch("urllib3.PoolManager")
+    def test_get_hashicorp_api_item_http_status_failure(self, manager):
+        manager.return_value = RequestPoolManager(HttpResponseMock(
+            constants.TEST_VERSIONS_GET_API_ITEM_RESPONSE_JSON, 404))
+        with self.assertRaises(Exception):
+            versions.get_latest_hashicorp_terraform_version()
+
+    @mock.patch("urllib3.PoolManager")
+    def test_get_hashicorp_api_item_item_not_found(self, manager):
+        manager.return_value = RequestPoolManager(HttpResponseMock(
+            constants.TEST_VERSIONS_GET_API_ITEM_RESPONSE_JSON))
+        with self.assertRaises(Exception):
+            versions.get_hashicorp_api_item("someurl", "someitem")
+
+
+class RequestPoolManager:
+
+    def __init__(self, response_mock):
+        self.response_mock = response_mock
+
+    def request(self, *args, **kwargs):
+        return self.response_mock
+
+
+class HttpResponseMock:
+
+    def __init__(self, data, status=200):
+        self.status = status
+        self.data = data
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
